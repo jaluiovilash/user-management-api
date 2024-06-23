69cdad1fd2ca69a2ee6199e92cef8a627c0e8bde
Ovilash <124290200+jaluiovilash@users.noreply.github.com>
2024-06-23 23:20:45 +0530

    Update README.d

--- README.d
+++ README.d
@@ -1,4 +1,4 @@
-<h1>User Management API</h1>
+# User Management API
 
 This is a simple Express.js application that provides a REST API for managing users. The application allows you to get a list of users, get user details by ID, create new users, update existing users, and delete users.
 
